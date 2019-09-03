7fa4c112aae08345082e23060fdc9cfc0973ddf3
vikkastaneja <vikas.vtaneja@gmail.com>
2019-09-02 17:34:02 -0700

    Converted return value to json string

--- ShapeApi/Controllers/ValuesController.cs
+++ ShapeApi/Controllers/ValuesController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ShapeApi.Shape;
-
+using ShapeApi.Json;
+using System.Runtime.Serialization.Json;
+using System.IO;
 
 namespace ShapeApi.Controllers
 {
@@ -14,32 +16,58 @@ namespace ShapeApi.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get(string shape, int param1, int param2)
+        public ActionResult<string> Get(string shape, int side1, int side2)
         {
+            var jshape = new JsonShape();
+            jshape.shape = shape.ToLower();
+            
+            jshape.side1 = side1;
+            jshape.side2 = side2;
+
             if (String.IsNullOrEmpty(shape))
             {
-                return new string[1] { "Unsupported shape" };
+                jshape.result = "Fail";
+                return GetResultString(jshape);
             }
 
-            int result = 0;
+            var result = 0;
             Shape.Shape s = null;
             switch (shape.ToLower())
             {
                 case "square":
-                    s = new Square(param1);
+                    s = new Square(side1 == 0 ? side2 : side1);
                     result = s.Area();
+                    jshape.result = "Success";
                     break;
                 case "rectangle":
-                    s = new Rectangle(param1, param2);
+                    s = new Rectangle(side1, side2);
                     result = s.Area();
+                    jshape.result = "Success";
                     break;
                 default:
-                    return new string[1]{ "Unsupported shape"};
+                    jshape.result = "Fail";
+                    break;
                     
             }
 
-            string [] returnValue = { "Area : " + result.ToString() };
-            return returnValue;
+            jshape.area = result;
+
+            return GetResultString(jshape);
+            
+        }
+
+        private string GetResultString(JsonShape jshape)
+        {
+            var stream1 = new MemoryStream();
+            var ser = new DataContractJsonSerializer(typeof(JsonShape));
+
+            ser.WriteObject(stream1, jshape);
+
+            stream1.Position = 0;
+            var sr = new StreamReader(stream1);
+
+
+            return sr.ReadToEnd();
         }
 
         //// GET api/values/5
---
+++ ShapeApi/Json/JsonShape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+namespace ShapeApi.Json
+{
+    [DataContract]
+    internal class JsonShape
+    {
+        [DataMember]
+        internal string shape;
+
+        [DataMember]
+        internal int area;
+
+        [DataMember]
+        internal int side1;
+
+        [DataMember]
+        internal int side2;
+
+        [DataMember]
+        internal string result;
+    }
+}
